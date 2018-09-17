127ade48f15890c7de2be40d65f267b359e40681
Dimitri Kohler <dkohler@Amirs-MacBook-Pro.local>
2018-09-17 22:56:27 +0200

    Added necessary component

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CustomerComponent } from './customer/customer.component';
+import { OwnerComponent } from './owner/owner.component';
+import { JudgeComponent } from './judge/judge.component';
+import { OfferComponent } from './offer/offer.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'customer/:id', component: CustomerComponent},
+  {path: 'owner/:id', component: OwnerComponent},
+  {path: 'judge/:id', component: JudgeComponent},
+  {path: 'offer', component: OfferComponent},
+  {path: '', redirectTo: 'offer', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { CustomerComponent } from './customer/customer.component';
+import { OwnerComponent } from './owner/owner.component';
+import { JudgeComponent } from './judge/judge.component';
+import { OfferComponent } from './offer/offer.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomerComponent,
+    OwnerComponent,
+    JudgeComponent,
+    OfferComponent
   ],
   imports: [
     BrowserModule,
