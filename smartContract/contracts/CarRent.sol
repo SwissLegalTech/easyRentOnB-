319317357d72b0df06de0bd1244b606158b66667
Andreas Hauri <hauri@mila.com>
2018-09-17 17:45:14 +0200

    added first rent smartcontract

--- smartContract/contracts/CarRent.sol
+++ smartContract/contracts/CarRent.sol
@@ -1,8 +1,11 @@
+pragma solidity ^0.4.24;
 
-contract CarRent {
+import "./Vat.sol";
+
+contract CarRent is Vat {
   address public owner;
   address public renter;
-  address public judge;
+  address public judge = 0xc61762175a7637B7623d999e7BBB1f74cF43dC0C;
   uint256 public rent = 20;
   uint256 public deposit = 100;
   uint256 public funds;
@@ -13,7 +16,7 @@ contract CarRent {
 
   }
 
-  function acceptCar() public payable {
+  function acceptCar() public whitelisted payable {
     require(renter == address(0));
     require(msg.value>=deposit+rent);
     renter = msg.sender;
@@ -23,39 +26,40 @@ contract CarRent {
   function returnCar() public {
     require(renter==msg.sender);
     uint256 rest = funds - rent;
-    renter.transfer(rest);
+    vatTransfer(owner, rest);
   }
 
   function returnDeposit(uint256 amount) public {
     require(deposit-amount>=0);
     require(owner==msg.sender);
     deposit -=  amount;
-    renter.transfer(amount);
+    transfer(renter, amount);
   }
 
   function disputeDeposit() {
-      require(renter==msg.sender);
-      require(!disputed);
-      require(deposit>0);
-      disputed = true;
+    require(renter==msg.sender);
+    require(!disputed);
+    require(deposit>0);
+    disputed = true;
   }
 
   function acceptDeposit() {
-      require(renter==msg.sender);
-      require(deposit>0);
-      disputed = false;
-      owner.transfer(deposit);
-      deposit = 0;
+    require(renter==msg.sender);
+    require(deposit>0);
+    disputed = false;
+    transfer(owner, deposit);
+    deposit = 0;
   }
 
   function judgeDeposit(uint256 ownerAmount) {
-      require(judge==msg.sender);
-      require(!disputed);
-      require(deposit>0);
-      require(ownerAmount<=deposit);
-      disputed = true;
-      owner.transfer(ownerAmount);
-      renter.transfer(deposit-ownerAmount);
+    require(judge==msg.sender);
+    require(!disputed);
+    require(deposit>0);
+    require(ownerAmount<=deposit);
+    disputed = true;
+    transfer(owner, ownerAmount);
+    transfer(renter, deposit-ownerAmount);
+    deposit=0;
   }
 
 }
